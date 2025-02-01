a16a4abc5c1e49cf044587dd422c179b1ced12cc
moshe mel <melamed2@gmail.com>
2025-02-01 12:43:17 +0200

    s2777ssdd22222333332223''גww'22222222554

--- functions/csvParser.js
+++ functions/csvParser.js
@@ -1,21 +1,19 @@
-
-
-  function extractGeneralData(data) {
+function extractGeneralData(data) {
     // 1. קבלת ימי עסקים שנותרו
-    const businessDaysLeft = data[0][data[0].length - 1] ? String(data[0][data[0].length - 1]).trim() : '0';
+    const businessDaysLeft = String(data[0][data[0].length - 1]).trim() || '0';
 
     // 2. אסוף את הנתונים הכלליים לשורות
-    const monthlyTargetPlatinum = data[data.length - 5] && data[data.length - 5][1] ? String(data[data.length - 5][1]).trim() : '0';
-    const monthlySalesPlatinum = data[data.length - 5] && data[data.length - 5][3] ? String(data[data.length - 5][3]).trim() : '0';
-    const dailyTargetPlatinum = data[data.length - 4] && data[data.length - 4][1] ? String(data[data.length - 4][1]).trim() : '0';
-    const dailySalesPlatinum = data[data.length - 4] && data[data.length - 4][3] ? String(data[data.length - 4][3]).trim() : '0';
+    const monthlyTargetPlatinum = String(data[data.length - 5][1]).trim() || '0';
+    const monthlySalesPlatinum = String(data[data.length - 5][3]).trim() || '0';
+    const dailyTargetPlatinum = String(data[data.length - 4][1]).trim() || '0';
+    const dailySalesPlatinum = String(data[data.length - 4][3]).trim() || '0';
 
-    const monthlyTargetMediho = data[data.length - 3] && data[data.length - 3][1] ? String(data[data.length - 3][1]).trim() : '0';
-    const monthlySalesMediho = data[data.length - 3] && data[data.length - 3][3] ? String(data[data.length - 3][3]).trim() : '0';
-    const dailyTargetMediho = data[data.length - 2] && data[data.length - 2][1] ? String(data[data.length - 2][1]).trim() : '0';
-    const dailySalesMediho = data[data.length - 2] && data[data.length - 2][3] ? String(data[data.length - 2][3]).trim() : '0';
+    const monthlyTargetMediho = String(data[data.length - 3][1]).trim() || '0';
+    const monthlySalesMediho = String(data[data.length - 3][3]).trim() || '0';
+    const dailyTargetMediho = String(data[data.length - 2][1]).trim() || '0';
+    const dailySalesMediho = String(data[data.length - 2][3]).trim() || '0';
 
-    const newsUpdates = data[data.length - 1] && data[data.length - 1][1] ? String(data[data.length - 1][1]).trim() : '';
+    const newsUpdates = String(data[data.length - 1][1]).trim() || '';
 
     // החזרת אובייקט עם הנתונים הכלליים
     return {
@@ -31,7 +29,6 @@
         newsUpdates
     };
 }
-
 
   export default {
     extractGeneralData
