5019b3209e0280f3be8efc7d3d705b2ccdadc458
moshe mel <melamed2@gmail.com>
2025-02-01 13:28:10 +0200

    s2777ssdd22222333332223''גww'222222225ג54

--- app.js
+++ app.js
@@ -7,38 +7,57 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // משתנה לאחסון הנתונים שהתקבלו
+let salesData = [];
+let generalData = [];
 
 app.use(cors());
-app.use(bodyParser.text({ type: '*/*' }));
-
-let webhookData = '[]';
-let salesData=[];
-let generalData=[];
+app.use(bodyParser.json());  // שים לב שפה אנחנו בוחרים ב-json
 
 // Endpoint שמאזין לבקשת GET בנתיב הראשי (לצורך הצגת הנתונים)
 app.get('/', (req, res) => {
-    res.send(`
-      <h1>הנתונים שהתקבלו:</h1>
-      <pre>
- ${JSON.stringify(webhookData, null, 2)} 
-        ${JSON.stringify(generalData, null, 2)}
-      </pre>
-    `);
-  });
+  res.send(`
+    <h1>הנתונים שהתקבלו:</h1>
+  `);
+});
+
+// Endpoint לשליפת הנתונים
+app.get('/general-data', (req, res) => {
+  // מחזיר את הנתונים שנשמרו
+  res.status(200).json(generalData);
+});
+
+// Endpoint לשליפת הנתונים
+app.get('/sales-data', (req, res) => {
+  // מחזיר את הנתונים שנשמרו
+  res.status(200).json(salesData);
+});
 
 // Endpoint שמאזין לנתונים לוובהוק
-app.post('/webhook', (req, res) => {
-    // עדכון המשתנה עם המידע שהתקבל מהוובהוק
-    webhookData = req.body;
-    generalData=extractGeneralData(webhookData);
-    // הדפסת המידע שהתקבל בקונסול של השרת
-    console.log(' התקבל מידע מהוובהוק:', webhookData);
+app.post('/webhook-sales-data', (req, res) => {
+  // עדכון המשתנה עם המידע שהתקבל מהוובהוק
+  const webhookSalesData = req.body;
+  salesData = webhookSalesData;
 
-    // שליחה של תשובה ללקוח שהמידע התקבל בהצלחה
-    res.send(`המידע שהתקבל: ${webhookData}`);
+  // הדפסת המידע שהתקבל בקונסול של השרת
+  console.log(' התקבל מידע מהוובהוק:', webhookSalesData);
+
+  // שליחה של תשובה ללקוח שהמידע התקבל בהצלחה
+  res.status(201).json({ message: 'Sales data received successfully!', data: webhookSalesData });
+});
+
+// Webhook לקבלת הנתונים
+app.post('/webhook-general', (req, res) => {
+  // קבל את הנתונים מהבקשה
+  const receivedGeneralData = req.body;
+
+  // שמור את הנתונים (במקרה הזה נשמר ב-memory)
+  generalData = receivedGeneralData;
+
+  // שלח תשובה שהנתונים התקבלו בהצלחה
+  res.status(201).json({ message: 'General data received successfully!', data: receivedGeneralData });
 });
 
 // הפעלת השרת
 app.listen(PORT, () => {
-    console.log(`🚀 השרת רץ על http://localhost:${PORT}`);
+  console.log(`🚀 השרת רץ על http://localhost:${PORT}`);
 });
